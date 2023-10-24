51651f9aebfbb74acd8786a62cf6eba8be4a7a25
Gin Seladipura <rawdep@gmail.com>
2023-10-25 00:29:44 +0700

    feat: auth and upload

--- src/lib/components/Listmenu.svelte
+++ src/lib/components/Listmenu.svelte
@@ -14,7 +14,7 @@
 <a
   href={link}
   on:click={handleClick}
-  class="text-center md:text-left flex flex-col md:flex-row items-center justify-center md:justify-start text-white border-b border-slate-500/20 pb-2 mb-2 last-of-type:border-0 cursor-pointer"
+  class="text-center md:text-left flex flex-col md:flex-row items-center justify-center md:justify-start text-white border-b border-slate-500/20 pb-2 mb-2 cursor-pointer"
 >
   <img src={`/${icon}`} alt={text} class="md:mr-2" />
   <span class="text-[10px] md:text-sm font">{text}</span>
